735453618523d271913af79d773e040b2c5bfc83
Exaltprecious <peculiarchristj1234@gmail.com>
2023-12-18 15:25:30 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - main block
@@ -8,14 +7,4 @@ int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-} <stdio.h>
-/**
- * main - main block
- * Return: 0
- */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
 }
-
