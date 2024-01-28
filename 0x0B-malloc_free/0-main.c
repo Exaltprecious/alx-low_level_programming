706c26653114fd9688dc942b5a87c331f7a491ad
Exaltprecious <peculiarchristj1234@gmail.com>
2024-01-28 08:34:25 -0800

    0-main.c

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -15,7 +15,7 @@ unsigned int i;
 
 i = 0;
 while (i < size)
-{
+}
 if (i % 10)
 {
 printf(" ");
@@ -23,7 +23,6 @@ printf(" ");
 if (!(i % 10) && i)
 {
 printf("\n");
-     
 }
 printf("0x%02x", buffer[i]);
 i++;
@@ -41,9 +40,12 @@ int main(void)
 char *buffer;
 
 buffer = create_array(98, 'H');
-    
 if  (buffer == NULL)
 {
 printf("failed to allocate memory\n");
 return (1);
-  }
+}
+simple_print_buffer(buffer, 98);
+free(buffer);
+return (0);
+}
