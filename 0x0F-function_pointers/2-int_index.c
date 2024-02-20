6d68f61c0c106ff6328b10935e603a336a0fa379
Exaltprecious <peculiarchristj1234@gmail.com>
2024-02-20 06:17:12 -0800

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -19,7 +19,7 @@ if (array && cmp)
 for (i = 0; i < size; i++)
 			         {
 if (cmp(array[i]) != 0)
-                                rreturn (i);
+                                return (i);
 }
 }
 
