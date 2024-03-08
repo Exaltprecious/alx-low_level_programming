8ed521b0b0b2a2e4d8a247b579c98fec26a3a418
Exaltprecious <peculiarchristj1234@gmail.com>
2024-03-07 20:50:37 -0800

    Function that prints 10x in lower cases

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - program entry starts here
+ * Return: 0 if success, non zero if fail
+ * _putchar: adds the characters
+ */
+void print_alphabet_x10(void)
+{
+	char la;
+	int ten;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+		{
+			_putchar (la);
+	if (la == 'z')
+		_putchar ('\n');
+	}
+}
+}
