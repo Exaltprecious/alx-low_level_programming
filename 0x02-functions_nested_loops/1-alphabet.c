e2f5d7a1e2ae750e5809f4882dde1123c9be188d
Exaltprecious <peculiarchristj1234@gmail.com>
2024-03-07 20:42:52 -0800

    Functions that rints alphabeth in lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
-#include "main.h"
+#include"main.h"
+
 /**
- *print_alphabet - abcd
- *Description: print lowercase alphabet
- *Return: void.
+ * print_alphabet - prints the alpha in lower case
+ * return: void  if successful, else 1
  */
 void print_alphabet(void)
-	{
-char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
-int i;
-for (i = 0; i < 26; i++)
 {
-_putchar(alphabet[i]);
-}
-_putchar('\n');
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		_putchar (la);
+	}
+	_putchar ('\n');
 }
